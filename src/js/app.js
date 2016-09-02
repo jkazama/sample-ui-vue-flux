781459b7a24f1a4370f9bf4ab16cebae408581af
junichiro_kazama <junichiro_kazama@xon.jp>
2016-09-02 18:11:02 +0900

    add store

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,16 @@
 
 // Vue / Vue Routing
 import Vue from 'vue'
+import store from './store'
 import router from './router'
+import { sync } from 'vuex-router-sync'
 
 // Register Global Event
 window.EventEmitter = new Vue()
+
+// sync the router with the vuex store.
+// this registers `store.state.route`
+sync(store, router)
 
 // Vue Custom Filter
 import * as filters from './filters'
