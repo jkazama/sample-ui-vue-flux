83bde7c4627eda9cbbb6b66f0030b4e540338b6a
Junichiro Kazama <kazama.junichiro@gmail.com>
2016-10-01 18:30:18 +0900

    Vue.js 2.0 対応

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createLogger from 'vuex/logger'
 
 import context from 'store/modules/context'
 import asset from 'store/modules/asset'
@@ -8,13 +7,11 @@ import asset from 'store/modules/asset'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
-const plugins = debug ? [createLogger] : []
 const store = new Vuex.Store({
   modules: {
     context,
     asset
   },
-  plugins: plugins,
   strict: debug
 })
 export default store
